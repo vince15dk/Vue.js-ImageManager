ef64c1964280bf342d65701a8e8cd30f8d6d26ac
SJ <vince15dk@gmail.com>
2019-02-13 20:18:41 +0900

    Vuex added

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<template>
+
+    <div class="ui secondary pointing menu">
+  <a href="/" class="item active">
+    Home
+  </a>
+  <a class="item">
+    Messages
+  </a>
+  <a class="item">
+    Friends
+  </a>
+  <div class="right menu">
+    <a class="ui item">
+      Login
+    </a>
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import Vue from 'vue';
+import App from './App';
+import store from './store/index';
 
 new Vue({
-  render: h => h(App),
+    store,
+    render: h => h(App)
 }).$mount('#app')
